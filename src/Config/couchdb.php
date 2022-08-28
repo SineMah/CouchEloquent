1d4c50620cb08569cabdee4779039a704ce1a686
SineMah <info@mincedmind.com>
2022-08-28 11:33:07 +0200

    update default config values

--- src/Config/couchdb.php
+++ src/Config/couchdb.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'session' => env('COUCHDB_USER', 300),
+    'session' => env('COUCHDB_SESSION', 300),
     'username' => env('COUCHDB_USER'),
     'password' => env('COUCHDB_PASSWORD'),
-    'database' => env('COUCHDB_DATABASE'),
-    'url' => env('COUCHDB_URL'),
+    'database' => env('COUCHDB_DATABASE', 'laravel'),
+    'url' => env('COUCHDB_URL', 'http://couchdb:5984'),
 ];
